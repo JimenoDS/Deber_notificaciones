bcf77527d242c20c88693f6cd4e9179bc1d20df7
JimenoDS <jajimenez.2@sudamericano.edu.ec>
2025-06-09 23:58:34 -0500

    otro cambio

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotificationsModule } from './notifications/notifications.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
- imports: [
- ConfigModule.forRoot({
- isGlobal: true,
- }),
- TypeOrmModule.forRoot({
- type: 'postgres',
- url: process.env.DATABASE_URL,
- port: 5432,
- username: 'postgres',
- password: 'cyxcHIHIcoqmJANNMiFpoelpfKdNtuaV',
- database: 'railway',
- autoLoadEntities: true,
- synchronize: true,
- }),
- NotificationsModule,
- ],
- controllers: [AppController],
- providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    NotificationsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
+export class AppModule {}
 
-
-export class AppModule {}
